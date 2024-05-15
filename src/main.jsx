32b4bbdab2b909c4840002ed04268dfe5150b3cf
mbobat1 <mbobat1@my.centennialcollege.ca>
2024-05-14 21:27:09 -0400

    ADD REACT ROUTER

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-
+import App from './App.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/legacy",
+    element: <App></App>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
