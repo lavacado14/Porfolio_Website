b9ff3123566251df9b34c7f7852969373839434d
mbobat1 <mbobat1@my.centennialcollege.ca>
2024-05-28 23:21:50 -0400

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; 
+import Navbar from './Navbar.jsx';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+
+// Define routing configuration
 const router = createBrowserRouter([
   {
-    path: "/legacy",
-    element: <App></App>,
+    path: "/", 
+    element: (
+      <>
+        <Navbar /> 
+        <div>Hello world!</div>
+      </>
+    ),
   },
+  
 ]);
+
+// Render the React application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
